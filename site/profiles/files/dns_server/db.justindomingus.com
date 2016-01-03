496739076322391336ee432943caa9417e9b46e2
Justin Domingus <domingusj@gmail.com>
2016-01-03 13:49:34 -0700

    add dns files

---
+++ site/profiles/files/dns_server/db.justindomingus.com
@@ -0,0 +1,28 @@
+;
+; BIND data file for local loopback interface
+;
+$TTL	604800
+@	IN	SOA	core1.justindomingus.com. admin.justindomingus.com. (
+			 010220161	; Serial
+			 604800		  ; Refresh
+			 86400		  ; Retry
+			 2419200		; Expire
+			 604800 )	  ; Negative Cache TTL
+;
+@	IN	NS	localhost.
+@	IN	A	127.0.0.1
+@	IN	AAAA	::1
+
+; name servers - NS records
+    IN      NS      core1.justindomingus.com.
+    IN      NS      core2.justindomingus.com.
+
+; name servers - A records
+core1.justindomingus.com.          IN      A       192.168.2.100
+core2.justindomingus.com.          IN      A       192.168.2.200
+
+; 192.168.2.0/24 - A records
+menacat1.justindomingus.com.    IN      A      192.168.2.101
+puppet1.justindomingus.com.     IN      A      192.168.2.102
+icinga1.justindomingus.com.     IN      A      192.168.2.103
+graphite1.justindomingus.com.   IN      A      192.168.2.104
--- site/profiles/manifests/dns_server.pp
+++ site/profiles/manifests/dns_server.pp
@@ -1,14 +1,40 @@
 #DNS server
 class profiles::dns_server {
 
-  package { 'tmux':
-    ensure => installed,
-  }
+$main_domain = hiera ('main_domain')
+$reverse_zone = hiera ('reverse_zone')
 
   firewall {'005 dns 53':
       dport  => '53',
       proto  => 'udp',
       action => 'accept',
+  }
+
+  package { 'bind9':
+    ensure => installed,
+  }
+
+  service { 'bind9':
+    ensure     => running,
+    enable     => true,
+    hasrestart => true,
+    hasstatus  => true,
+  }
+
+  file { 'forward_dns':
+    ensure => file,
+    path   => "${etc/zones/db.$main_domain}",
+    mode   => '0644',
+    source => "puppet:///modules/profiles/dns_server/db.$main_domain",
+    notify => Service['bind9'],
+  }
+
+  file { 'reverse_dns':
+    ensure => file,
+    path   => "${etc/zones/db.$reverse_zone}",
+    mode   => '0644',
+    source => "puppet:///modules/profiles/dns_server/db.$reverse_zone",
+    notify => Service['bind9'],
   }
 
 }
