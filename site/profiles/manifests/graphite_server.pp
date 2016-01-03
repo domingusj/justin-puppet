5d546992a2c6a21c8818936c4e524afdeffe80b2
Justin Domingus <domingusj@gmail.com>
2016-01-03 12:24:44 -0700

    add firewall for graphite and icinga

--- site/profiles/manifests/graphite_server.pp
+++ site/profiles/manifests/graphite_server.pp
@@ -3,4 +3,16 @@ class profiles::graphite_server {
 
   class { 'graphite': }
 
+  firewall {'100 web 80':
+      dport  => '80',
+      proto  => 'tcp',
+      action => 'accept',
+  }
+
+  firewall {'101 web 443':
+      dport  => '443',
+      proto  => 'tcp',
+      action => 'accept',
+  }
+
 }
--- site/profiles/manifests/icinga_server.pp
+++ site/profiles/manifests/icinga_server.pp
@@ -3,4 +3,16 @@ class profiles::icinga_server {
 
   include icingaweb2
 
+  firewall {'100 web 80':
+      dport  => '80',
+      proto  => 'tcp',
+      action => 'accept',
+  }
+
+  firewall {'101 web 443':
+      dport  => '443',
+      proto  => 'tcp',
+      action => 'accept',
+  }
+
 }
