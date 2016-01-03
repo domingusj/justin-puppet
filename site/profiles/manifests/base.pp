71d7e65a9945d91796b50dc74e6627c5c149929b
Justin Domingus <domingusj@gmail.com>
2016-01-03 09:31:56 -0700

    rename profiles

---
+++ site/profiles/manifests/base.pp
@@ -0,0 +1,30 @@
+#base profile everyone gets
+
+class profiles::base {
+
+  # common users
+  users { 'common': }
+
+  class { '::ntp': }
+
+  # set locale
+  class { 'locales':
+      default_locale => 'en_US.UTF-8',
+      locales        => ['en_US.UTF-8 UTF-8'],
+  }
+
+  # base firewall config
+  include profiles::firewall::setup
+
+  # common packages needed everywhere
+  package {[
+          'vim',
+          'sudo',
+          'screen',
+          'tree',
+          'htop'
+      ]:
+      ensure => present,
+  }
+
+}
