439f46ec07c548d15dec95cb6562724de49fb71b
Justin Domingus <domingusj@gmail.com>
2016-01-04 20:08:25 -0700

    add puppetdb to master

--- site/profiles/manifests/puppetserver_server.pp
+++ site/profiles/manifests/puppetserver_server.pp
@@ -1,10 +1,12 @@
 #Puppetserver server
 class profiles::puppetserver_server {
-  
+
   firewall { '100 puppet 8140':
     dport  => '8140',
     proto  => 'tcp',
     action => 'accept',
   }
+
+  include puppetdb
 
 }
