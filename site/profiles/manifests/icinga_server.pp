b5df3644cc0b4575b479bac9acff5d78d3edcacf
Justin Domingus <domingusj@gmail.com>
2016-01-04 20:57:24 -0700

    add icinga db pw

--- site/profiles/manifests/icinga_server.pp
+++ site/profiles/manifests/icinga_server.pp
@@ -1,8 +1,18 @@
 #Icinga server
 class profiles::icinga_server {
 
+  $icinga_db_pw = hiera ('icinga_db_pw')
+
   include profiles::firewall::web_fw
-  
+
   include icingaweb2
+
+
+  class { 'postgresql::server': }
+
+  postgresql::server::db { 'icinga2_data':
+    user     => 'icinga2',
+    password => postgresql_password('icinga2', $icinga_db_pw),
+  }
 
 }
