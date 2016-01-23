881c9fedc63117e606065e2dce57ae0a49dfbd6c
Justin Domingus <domingusj@gmail.com>
2016-01-23 15:42:05 -0700

    add sensu config

--- site/profiles/manifests/sensu_server.pp
+++ site/profiles/manifests/sensu_server.pp
@@ -8,7 +8,22 @@ class profiles::sensu_server {
     source => hiera('private_network'),
   }
 
-  class { 'sensu': }
+  class { 'sensu':
+    plugins => [
+      'puppet:///data/sensu/plugins/ntp.rb',
+      'puppet:///data/sensu/plugins/postfix.rb'
+    ],
+  }
+
+  sensu::handler { 'default':
+    command => 'mail -s \'sensu alert\' domingusj@gmail.com.com',
+  }
+
+  sensu::check { 'check_ntp':
+    command     => 'PATH=$PATH:/usr/lib/nagios/plugins check_ntp_time -H pool.ntp.org -w 30 -c 60',
+    handlers    => 'default',
+    subscribers => 'sensu-test',
+  }
 
   include 'erlang'
 
