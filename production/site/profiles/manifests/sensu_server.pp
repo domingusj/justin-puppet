ef634a34d7fee5b0e52e0631c58370c3f39d73ff
Justin Domingus <domingusj@gmail.com>
2017-01-30 10:28:41 -0800

    Update sensu_server.pp

--- production/site/profiles/manifests/sensu_server.pp
+++ production/site/profiles/manifests/sensu_server.pp
@@ -8,12 +8,12 @@ class profiles::sensu_server {
     source => hiera('private_network'),
   }
 
-  class { 'sensu':
-    plugins => [
-      'puppet:///data/sensu/plugins/ntp.rb',
-      'puppet:///data/sensu/plugins/postfix.rb'
-    ],
-  }
+ # class { 'sensu':
+ #   plugins => [
+ #     'puppet:///data/sensu/plugins/ntp.rb',
+ #     'puppet:///data/sensu/plugins/postfix.rb'
+ #   ],
+ # }
 
   sensu::handler { 'default':
     command => 'mail -s \'sensu alert\' domingusj@gmail.com.com',
